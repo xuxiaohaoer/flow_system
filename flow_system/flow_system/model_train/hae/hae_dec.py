ac8e22cf33c4d1b039cbc8c7ab3a711ad6a4409c
sunny77889 <1074913189@qq.com>
2021-09-10 21:02:04 +0800

    MT模型测试添加展示的原始特征

--- flow_system/flow_system/model_train/hae/hae_dec.py
+++ flow_system/flow_system/model_train/hae/hae_dec.py
@@ -15,9 +15,35 @@ from .tools import read_dataset, evaluation, evaluation_types
 from time import time
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+raw_features= [' Flow Duration',' Total Fwd Packets',' Total Backward Packets',
+               'Total Length of Fwd Packets',' Total Length of Bwd Packets',
+               ' Fwd Packet Length Max',' Fwd Packet Length Min',
+               ' Fwd Packet Length Mean',' Fwd Packet Length Std',
+               'Bwd Packet Length Max',' Bwd Packet Length Min',
+               ' Bwd Packet Length Mean',' Bwd Packet Length Std','Flow Bytes/s',
+               ' Flow Packets/s',' Flow IAT Mean',' Flow IAT Std',' Flow IAT Max',
+               ' Flow IAT Min','Fwd IAT Total',' Fwd IAT Mean',' Fwd IAT Std',
+               ' Fwd IAT Max',' Fwd IAT Min','Bwd IAT Total',' Bwd IAT Mean',
+               ' Bwd IAT Std',' Bwd IAT Max',' Bwd IAT Min','Fwd PSH Flags',
+               ' Bwd PSH Flags',' Fwd URG Flags',' Bwd URG Flags',' Fwd Header Length',
+               ' Bwd Header Length','Fwd Packets/s',' Bwd Packets/s',
+               ' Min Packet Length',' Max Packet Length',' Packet Length Mean',
+               ' Packet Length Std',' Packet Length Variance','FIN Flag Count',
+               ' SYN Flag Count',' RST Flag Count',' PSH Flag Count',' ACK Flag Count',
+               ' URG Flag Count',' CWE Flag Count',' ECE Flag Count',' Down/Up Ratio',
+               ' Average Packet Size',' Avg Fwd Segment Size',' Avg Bwd Segment Size',
+               'Fwd Avg Bytes/Bulk',' Fwd Avg Packets/Bulk',' Fwd Avg Bulk Rate',
+               ' Bwd Avg Bytes/Bulk',' Bwd Avg Packets/Bulk','Bwd Avg Bulk Rate',
+               'Subflow Fwd Packets',' Subflow Fwd Bytes',' Subflow Bwd Packets',
+               ' Subflow Bwd Bytes','Init_Win_bytes_forward',' Init_Win_bytes_backward',
+               ' act_data_pkt_fwd',' min_seg_size_forward','Active Mean',' Active Std',
+               ' Active Max',' Active Min','Idle Mean',' Idle Std',' Idle Max',
+               ' Idle Min',' Protocol_0.0',' Protocol_6.0',' Protocol_17.0',' Source IP',' Source Port',' Destination IP',' Destination Port',' Label','types']
+
 
 train_path = "./data_feature/HAE/train_data.npy"
 test_path = "./data_feature/HAE/test_data.npy"
+raw_test_path="./data_feature/HAE/raw_test.npy"
 saved_model_path = './modelSaved/hae/'
 
 def HAE_train():
@@ -35,7 +61,7 @@ def HAE_train():
     print("HAE Train Time:".format(time() - t1))
     print("Saving Model......")
     clf.save(saved_model_path)
-
+import pandas as pd
 
 def HAE_test():
     # 模型训练与测试
@@ -58,6 +84,15 @@ def HAE_test():
     test_binary_labels = np.array(test_labels).astype(int)
     pre_data = pre_data.astype(int)  # 测试样本预测值，0，1序列
     test_ip_port = test_ids[:, :4]  # 源ip，源port，目的IP、目的port
+
+    raw_data = np.load(raw_test_path,allow_pickle=True) # 测试数据对应的原始特征值（归一化之前）
+    raw_df=pd.DataFrame(raw_data,columns=raw_features)
+    #展示的特征：流持续时间,上⾏流数据包总数,下⾏流数据包总数,上⾏数据包总⻓度,下⾏数据包总⻓度
+    #每秒流字节数，每秒流包数，FIN包数量，SYN包数量，RST包数量，PSH包数量，ACK包数量
+    #URG包数量，CWR包数量，ECE包数量，下载上传⽐率，包平均⼤⼩
+    view_feas=raw_df[[' Flow Duration',' Total Fwd Packets',' Total Backward Packets','Total Length of Fwd Packets',' Total Length of Bwd Packets',
+                      'Flow Bytes/s',' Flow Packets/s','FIN Flag Count',' SYN Flag Count',' RST Flag Count',' PSH Flag Count',' ACK Flag Count',
+                      ' URG Flag Count',' CWE Flag Count',' ECE Flag Count',' Down/Up Ratio',' Average Packet Size']]
     
     from model_test.models import FlowRes
     for i in range(len(pre_data)):
