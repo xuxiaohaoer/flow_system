20a28966ea62627b1f21985e8969b330e7a99d87
xuxiaohaoer <bzxuhao@163.com>
2021-09-15 09:50:10 +0800

    修改：9-15-1

--- flow_system/flow_system/flow_cut/views.py
+++ flow_system/flow_system/flow_cut/views.py
@@ -5,10 +5,7 @@ def cut(request):
     num_tot_flow, num_tot_tls = flow_pre_cut()
     while (len(num_tot_flow)<5):
         num_tot_flow.append(0)
-        num_tot_tls.append(0)
-
-   
-   
+        num_tot_tls.append(0)   
     return render(request, 'flow_cut/cut.html', {"num_tls":num_tot_tls,
                                                  "num_flow":num_tot_flow
     })
